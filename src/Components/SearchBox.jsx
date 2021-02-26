bf9c453ea65ed719e4d249f1e6b5bb36c01790fb
bestedicte <benedictevikvam@hotmail.com>
2021-02-26 15:06:23 +0100

    did some smaller fixes

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -1,10 +1,11 @@
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
 	return (
 		<div className='pa2'>
 			<input
 				type='search'
 				placeholder='search robots'
 				className='pa3 ba b--green bg-lightest-blue'
+				onChange={searchChange}
 			/>
 		</div>
 	);
